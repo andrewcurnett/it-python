ba24570821d89a9b1aa88320aee05c845f4d82f3
Andrew Curnett <andrew.curnett@gmail.com>
2019-09-26 14:02:35 -0400

    Made it look more pretty

--- htmlcount.py
+++ htmlcount.py
@@ -1,29 +1,29 @@
 from banner import banner
 import os
+def load(filename):
+    data = []
+    print(f"..........loading from {filename}")
+    if os.path.exists(filename):
+        with open(filename) as fin:
+            for entry in fin.readlines():
+                data.append(entry.rstrip())
 
+    return data
+
+def html_tag_counter(text):
+    text = str(text)
+    tag_count = 0
+    previous_char = None
+    for char in text:
+        if char != "/" and previous_char == "<":
+            tag_count += 1
+        previous_char = char
+    return tag_count
 banner("HTML TAG COUNTER", "Andrew Curnett")
 again = 'Y'
 print("Welcome to the HTML tag counter")
 while again:
-    def load(filename):
-        data = []
-        print(f"..........loading from {filename}")
-        if os.path.exists(filename):
-            with open(filename) as fin:
-                for entry in fin.readlines():
-                    data.append(entry.rstrip())
 
-        return data
-
-    def html_tag_counter(text):
-        text = str(text)
-        tag_count = 0
-        previous_char = None
-        for char in text:
-            if char != "/" and previous_char == "<":
-                tag_count += 1
-            previous_char = char
-        return tag_count
 
 
 
@@ -35,9 +35,9 @@ while again:
     again = input("Would you like to count another HTML file?(Y/N) ")
     print('')
 
-    if again.upper == 'Y':
+    if again.upper() == 'Y':
         continue
 
-    if again.upper == 'N':
+    if again.upper() == 'N':
         print('\n' * 100)
-
+        break
